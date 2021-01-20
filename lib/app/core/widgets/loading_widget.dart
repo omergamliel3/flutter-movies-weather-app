4ccbaeae180e5d7abbd7aa5d357ba9bb7d187a75
Omer Gamliel <omergamliel3@gmail.com>
2021-01-21 00:12:51 +0200

    add const constructor

--- lib/app/core/widgets/loading_widget.dart
+++ lib/app/core/widgets/loading_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
+  const LoadingWidget();
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).accentColor;
