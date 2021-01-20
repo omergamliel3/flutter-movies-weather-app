0981d8aa32174f47ddc5ba89cdfdbd54a5f66ee9
Omer Gamliel <omergamliel3@gmail.com>
2021-01-21 00:00:44 +0200

    add view exports

--- lib/app/features/movies/presentation/index.dart
+++ lib/app/features/movies/presentation/index.dart
@@ -1,2 +1,5 @@
 export './home_page/view/home_view.dart';
 export './loading_page/view/loading_view.dart';
+export './main_page/view/main_view.dart';
+export './search_page/view/search_view.dart';
+export './saved_page/view/saved_view.dart';
