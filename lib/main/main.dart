cf866476c040151cafbfaf393525eee684558346
Omer Gamliel <omergamliel3@gmail.com>
2021-01-20 20:33:30 +0200

    update materialApp title

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -9,6 +9,7 @@ import '../app/theme/theme.dart';
 import '../app/routes/app_routes.dart';
 
 import '../di/injector.dart';
+import '../localization/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           builder: (context, state) {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: 'title',
+              title: AppLocalizations.of(context).translate(APP_TITLE),
               theme: lightThemeData(),
               darkTheme: darkThemeData(),
               themeMode: state.when(
