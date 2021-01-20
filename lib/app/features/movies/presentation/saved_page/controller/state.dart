98f9319af4c2132add39501ffc9b44dea9555671
Omer Gamliel <omergamliel3@gmail.com>
2021-01-21 00:13:45 +0200

    setup saved view controller

--- lib/app/features/movies/presentation/saved_page/controller/state.dart
+++ lib/app/features/movies/presentation/saved_page/controller/state.dart
@@ -1,12 +1,9 @@
 // import 'package:flutter/foundation.dart';
 // import 'package:freezed_annotation/freezed_annotation.dart';
 
-// import '../../../../../core/errors/failure.dart';
-
 // part 'state.freezed.dart';
 
 // @freezed
-// abstract class LoadingState with _$LoadingState {
-//   const factory LoadingState.loading() = Loading;
-//   const factory LoadingState.error(Failure failure) = Error;
-// }
+// abstract class SavedState with _$SavedState {}
+
+class SavedState {}
