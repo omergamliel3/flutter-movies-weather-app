a27d2a0b244a5ba31fe8e18b6bfa533048d125a4
Omer Gamliel <omergamliel3@gmail.com>
2021-01-22 18:28:31 +0200

    update api endpoint

--- lib/app/features/movies/data/api/weather_api.dart
+++ lib/app/features/movies/data/api/weather_api.dart
@@ -10,8 +10,8 @@ abstract class WeatherRestClient {
   factory WeatherRestClient(Dio dio, {String baseUrl}) = _WeatherRestClient;
 
   @GET("/weather?lat={lat}&lon={lon}&units=metric&appid={apikey}")
-  Future<WeatherModel> getWeatherByCoords(@Path('lat') String lat,
-      @Path('lon') String lon, @Path('apikey') String apikey);
+  Future<WeatherModel> getWeatherByCoords(@Path('lat') double lat,
+      @Path('lon') double lon, @Path('apikey') String apikey);
 
   @GET("/weather?q={city}&units=metric&appid={apikey}")
   Future<WeatherModel> getWeatherByCity(
