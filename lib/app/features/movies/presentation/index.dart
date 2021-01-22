f754140069d9a7abef6cf7c585ffd1dcacad31df
Omer Gamliel <omergamliel3@gmail.com>
2021-01-22 16:01:15 +0200

    add weather view export

--- lib/app/features/movies/presentation/index.dart
+++ lib/app/features/movies/presentation/index.dart
@@ -3,3 +3,4 @@ export './loading_page/view/loading_view.dart';
 export './main_page/view/main_view.dart';
 export './search_page/view/search_view.dart';
 export './saved_page/view/saved_view.dart';
+export './weather_page/view/weather_view.dart';
