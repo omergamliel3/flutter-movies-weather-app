eecb5c49d45326789b51ed35095693f55720e22f
Omer Gamliel <omergamliel3@gmail.com>
2021-01-22 17:52:48 +0200

    add keys contants

--- lib/app/features/movies/data/models/weather_model.dart
+++ lib/app/features/movies/data/models/weather_model.dart
@@ -2,6 +2,18 @@ import 'package:meta/meta.dart';
 
 import '../../domain/entities/weather.dart';
 
+const MAIN_KEY = 'main';
+const DESCRIPTION_KEY = 'description';
+const TEMP_KEY = 'temp';
+const FEELS_LIKE_KEY = 'feels_like';
+const TEMP_MIN_KEY = 'temp_min';
+const TEMP_MAX_KEY = 'temp_max';
+const HUMIDITY_KEY = 'humidity';
+const WIND_SPEED_KEY = 'speed';
+const WIND_DEG_KEY = 'deg';
+
+const UNKNOWN = 'unknown';
+
 class WeatherModel extends Weather {
   const WeatherModel({
     @required String main,
@@ -31,43 +43,43 @@ class WeatherModel extends Weather {
     final windData = json['wind'] as Map<String, dynamic>;
 
     return WeatherModel(
-      main: weatherData['main'] as String,
-      description: weatherData['description'] as String,
-      temp: mainData['temp'] as double,
-      feelsLike: mainData['feels_like'] as double,
-      tempMin: mainData['temp_min'] as double,
-      tempMax: mainData['temp_max'] as double,
-      humidity: mainData['humidity'] as double,
-      windSpeed: windData['speed'] as double,
-      windDeg: windData['deg'] as double,
+      main: weatherData[MAIN_KEY] as String ?? UNKNOWN,
+      description: weatherData[DESCRIPTION_KEY] as String ?? UNKNOWN,
+      temp: mainData[TEMP_KEY] as double,
+      feelsLike: mainData[FEELS_LIKE_KEY] as double,
+      tempMin: mainData[TEMP_MIN_KEY] as double,
+      tempMax: mainData[TEMP_MAX_KEY] as double,
+      humidity: mainData[HUMIDITY_KEY] as double,
+      windSpeed: windData[WIND_SPEED_KEY] as double,
+      windDeg: windData[WIND_DEG_KEY] as double,
     );
   }
 
   factory WeatherModel.fromJsonCache(Map<String, dynamic> json) {
     return WeatherModel(
-      main: json['main'] as String,
-      description: json['description'] as String,
-      temp: json['temp'] as double,
-      feelsLike: json['feels_like'] as double,
-      tempMin: json['temp_min'] as double,
-      tempMax: json['temp_max'] as double,
-      humidity: json['humidity'] as double,
-      windSpeed: json['speed'] as double,
-      windDeg: json['deg'] as double,
+      main: json[MAIN_KEY] as String,
+      description: json[DESCRIPTION_KEY] as String,
+      temp: json[TEMP_KEY] as double,
+      feelsLike: json[FEELS_LIKE_KEY] as double,
+      tempMin: json[TEMP_MIN_KEY] as double,
+      tempMax: json[TEMP_MAX_KEY] as double,
+      humidity: json[HUMIDITY_KEY] as double,
+      windSpeed: json[WIND_SPEED_KEY] as double,
+      windDeg: json[WIND_DEG_KEY] as double,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      "main": main,
-      "description": description,
-      "temp": temp,
-      "feels_like": feelsLike,
-      "temp_min": tempMin,
-      "temp_max": tempMax,
-      "humidity": humidity,
-      "speed": windSpeed,
-      "deg": windDeg,
+      MAIN_KEY: main,
+      DESCRIPTION_KEY: description,
+      TEMP_KEY: temp,
+      FEELS_LIKE_KEY: feelsLike,
+      TEMP_MIN_KEY: tempMin,
+      TEMP_MAX_KEY: tempMax,
+      HUMIDITY_KEY: humidity,
+      WIND_SPEED_KEY: windSpeed,
+      WIND_DEG_KEY: windDeg,
     };
   }
 }
