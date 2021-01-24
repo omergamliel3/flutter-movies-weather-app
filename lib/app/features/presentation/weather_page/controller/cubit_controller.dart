f623d74ab8fbf1ba8ac1d6004c26618433a36d7e
Omer Gamliel <omergamliel3@gmail.com>
2021-01-24 13:29:25 +0200

    handle web location

--- lib/app/features/presentation/weather_page/controller/cubit_controller.dart
+++ lib/app/features/presentation/weather_page/controller/cubit_controller.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:flutter/foundation.dart' as foundation;
 
 import 'package:prospera_exercise/app/core/errors/failure.dart';
 import 'package:prospera_exercise/app/core/services/location.dart';
@@ -10,7 +13,7 @@ import 'package:prospera_exercise/app/features/weather/domain/usecases/get_remot
 
 import 'state.dart';
 
-const ERROR_MSG = 'Device location is unknown';
+const ERROR_MSG = 'The GPS signal is unavailable';
 
 class WeatherViewController extends Cubit<WeatherState> {
   WeatherViewController({
@@ -24,8 +27,20 @@ class WeatherViewController extends Cubit<WeatherState> {
   Future<void> getWeather() async {
     // emit loading state
     emit(const Loading());
-    // get device location
-    final location = await locationService.getDeviceLocation();
+
+    Position location;
+
+    //! handle web location (location does not supported in web platform)
+    if (foundation.kIsWeb) {
+      // assgin london city location
+      location = Position(longitude: -0.118092, latitude: 51.509865);
+    }
+    //! handle ios / android location
+    else {
+      // get device location from service
+      location = await locationService.getDeviceLocation();
+    }
+
     // emit error state if location is null
     if (location == null) {
       emit(const Error(Failure(ERROR_MSG)));
@@ -36,6 +51,7 @@ class WeatherViewController extends Cubit<WeatherState> {
         lon: location.longitude,
       ));
 
+      // fold result and emit state
       emit(result.fold(
         (failure) => Error(failure),
         (weather) => Success(weather),
