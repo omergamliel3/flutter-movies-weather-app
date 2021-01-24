46937453e3a72e11bc88dc5a6707e30ed2b0438e
Omer Gamliel <omergamliel3@gmail.com>
2021-01-24 22:59:27 +0200

    final project wrap up

--- lib/app/features/presentation/weather_page/widgets/error_view.dart
+++ lib/app/features/presentation/weather_page/widgets/error_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:prospera_exercise/app/core/errors/failure.dart';
 
 class ErrorView extends StatelessWidget {
@@ -6,12 +7,22 @@ class ErrorView extends StatelessWidget {
   const ErrorView(this.failure);
   @override
   Widget build(BuildContext context) {
-    const style = TextStyle(fontSize: 25);
     return Center(
-      child: Text(
-        failure.message,
-        style: style,
-      ),
-    );
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          failure.message,
+          style: const TextStyle(fontSize: 30),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Icon(
+          Icons.wifi_off,
+          size: 40,
+        ),
+      ],
+    ));
   }
 }
