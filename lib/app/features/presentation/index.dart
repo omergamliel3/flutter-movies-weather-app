d70521d27687ddcf8eefa62bb27820d4494b894f
Omer Gamliel <omergamliel3@gmail.com>
2021-01-24 14:55:58 +0200

    add seach view export

--- lib/app/features/presentation/index.dart
+++ lib/app/features/presentation/index.dart
@@ -2,3 +2,4 @@ export './home_page/view/home_view.dart';
 export './loading_page/view/loading_view.dart';
 export './main_page/view/main_view.dart';
 export './weather_page/view/weather_view.dart';
+export './search_page/view/search_view.dart';
