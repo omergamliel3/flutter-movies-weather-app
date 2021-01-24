1953dc90744b442ce365acda9a4f51d8086fadf8
Omer Gamliel <omergamliel3@gmail.com>
2021-01-24 14:55:48 +0200

    update theme data

--- lib/app/theme/theme_data.dart
+++ lib/app/theme/theme_data.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 // Light theme data
 ThemeData lightThemeData() {
-  return ThemeData.light();
+  return ThemeData.light().copyWith(
+      inputDecorationTheme:
+          const InputDecorationTheme(border: InputBorder.none));
 }
 
 // Dark theme data
 ThemeData darkThemeData() {
-  return ThemeData.dark();
+  return ThemeData.dark().copyWith(
+      inputDecorationTheme:
+          const InputDecorationTheme(border: InputBorder.none));
 }
